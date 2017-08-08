3a884e5e1da600427a7fbb12ca00212fa77d87c5
pipop1150 <pppiiipppoooppp@hotmail.com>
2017-08-08 13:53:36 +0700

    update config

--- config/database.php
+++ config/database.php
@@ -6,15 +6,15 @@ include $root."application/config/config.php";
 include $root."application/config/database.php";
 
 
-$host = $db['default']['hostname'];
-$userdb = $db['default']['username'];
-$passworddb = $db['default']['password'];
-$dbuse = $db['default']['database'];
+// $host = $db['default']['hostname'];
+// $userdb = $db['default']['username'];
+// $passworddb = $db['default']['password'];
+// $dbuse = $db['default']['database'];
 
-// $host = 'localhost';
-// $userdb = 'www';
-// $passworddb = 'webrkl@word';
-// $dbuse = 'sjuwebdb';
+$host = 'localhost';
+$userdb = 'www';
+$passworddb = 'webrkl@word';
+$dbuse = 'sjuwebdb';
 
 $conn = mysql_connect($host,$userdb,$passworddb);
 mysql_select_db($dbuse) or die(mysql_error());
